94a7b3ec30c66e871630c8d3bc83dc6ec8c7de82
potatoFinn <159907040+potatoFinn@users.noreply.github.com>
2024-02-13 15:29:09 +0100

    Test for item save
    alle saves moeten met knop

--- Rogier de Avonturier 2/Assets/Code/SaveAndLoadTest.cs
+++ Rogier de Avonturier 2/Assets/Code/SaveAndLoadTest.cs
@@ -7,22 +7,35 @@ using UnityEngine.UI;
 public class SaveAndLoadTest : MonoBehaviour
 {
     public TMP_InputField inputField;
+    public ItemsTest itemtest;
 
 
 
     public void saveData()
     {
         PlayerPrefs.SetString("input", inputField.text);
+        print("gay");
+        PlayerPrefs.SetInt("item", itemtest.item);
+        print("luk");
     }
 
 
     public void loadData() 
     {
         inputField.text = PlayerPrefs.GetString("input");
+        itemtest.item = PlayerPrefs.GetInt("item");
     }
 
     public void DeleteData()
     {
         PlayerPrefs.DeleteKey("input");
+    }
+    public void Start()
+    {
+        
+    }
+    public void startgame()
+    {
+        itemtest.item = PlayerPrefs.GetInt("items");
     }
 }

3124d1949125380618c91f56f639184c15ae8b5f
potatoFinn <159907040+potatoFinn@users.noreply.github.com>
2024-02-18 19:16:18 +0100

    jump

--- Rogier de Avonturier 2/Assets/Code/Walking.cs
+++ Rogier de Avonturier 2/Assets/Code/Walking.cs
@@ -8,10 +8,13 @@ public class walking : MonoBehaviour
     public float hor;
     public Vector3 movedir;
     public float speed;
+    private Rigidbody rb;
+    public float jumpforce = 5f;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+          rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -22,7 +25,21 @@ public class walking : MonoBehaviour
         movedir.z = vert;
         movedir.x = hor;
         transform.Translate(movedir * Time.deltaTime * speed);
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump();       
+        }
+    }
+    void Jump()
+    {
+        if (Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            // Apply an impulse force in the upward direction
+            rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }
     }
 }
+    
+
 
 

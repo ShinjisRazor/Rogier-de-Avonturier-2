11ecdd7f9edc2b1c600682dbc91dc430e465380b
potatoFinn <159907040+potatoFinn@users.noreply.github.com>
2024-03-18 11:40:29 +0100

    start pause menu code

---
+++ Rogier de Avonturier 2/Assets/Code/Ui/Pause menu/Pausemenu.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Pausemenu : MonoBehaviour
+{
+    public Canvas pause;
+    public Canvas settings;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            
+        }
+    }
+}

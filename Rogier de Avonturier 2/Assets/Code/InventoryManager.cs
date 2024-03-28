37e34921c686172d2289c429dc4886009ce89140
Omar Magour <omg080902@gmail.com>
2024-03-28 15:08:54 +0100

    Inventory script klaar voor laatste tests

--- Rogier de Avonturier 2/Assets/Code/InventoryManager.cs
+++ Rogier de Avonturier 2/Assets/Code/InventoryManager.cs
@@ -8,6 +8,7 @@ public class InventoryManager : MonoBehaviour
 
     public GameObject inventoryMenu;
     private bool menuActivated;
+    public ItemSlot[] itemSlot;
     
     void Start()
     {
@@ -35,6 +36,22 @@ public class InventoryManager : MonoBehaviour
     }
     public void AddItem(string itemName, int quantity, Sprite itemSprite)
     {
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            if (itemSlot[i].filled == false)
+            {
+                itemSlot[i].AddItem(itemName, quantity, itemSprite);
+                return;
+            }
+        }
+    }
 
+    public void DeselectAllSlots()
+    {
+        for(int i = 0; i < itemSlot.Length; i++)
+        {
+            itemSlot[i].selectedShader.SetActive(false);
+            itemSlot[i].thisItemSelected = false;
+        }
     }
 }
--- Rogier de Avonturier 2/Assets/Code/ItemSlot.cs
+++ Rogier de Avonturier 2/Assets/Code/ItemSlot.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class ItemSlot : MonoBehaviour
+public class ItemSlot : MonoBehaviour, IPointerClickHandler
 {
     public string itemName;
     public int quantity;
@@ -15,15 +16,46 @@ public class ItemSlot : MonoBehaviour
     private TMP_Text quantityText;
 
     [SerializeField]
-    private Image 
+    private Image itemImage;
 
-    void Start()
+    public GameObject selectedShader;
+    public bool thisItemSelected;
+
+    private InventoryManager inventoryManager;
+
+    private void Start()
     {
-        
+        inventoryManager = GameObject.Find("Inventory Canvas").GetComponent<InventoryManager>();
     }
 
-    void Update()
+    public void AddItem(string itemName, int quantity, Sprite itemSprite)
     {
-        
+        this.itemName = itemName;
+        this.quantity = quantity;  
+        this.itemSprite = itemSprite;
+        filled = true;
+
+        quantityText.text = quantity.ToString();
+        quantityText.enabled = true;
+        itemImage.sprite = itemSprite;
+        itemImage.enabled = true;
+
+    }
+
+
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.button == PointerEventData.InputButton.Left) 
+        {
+            OnleftClick();
+        }
+    }
+
+    public void OnleftClick()
+    {
+        inventoryManager.DeselectAllSlots();
+        selectedShader.SetActive(true);
+        thisItemSelected = true;
     }
 }
